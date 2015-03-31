eff73d4e738444edbbc4ebde4035ef99f18d9a86
12301025-YANGYuwei <12301025@bjtu.edu.cn>
2015-03-31 21:50:33 +0800

    version0.5

---
+++ WordStudy-version0.5/app/src/main/java/wordroid/Explanation/AboutFragment1.java
@@ -0,0 +1,69 @@
+package wordroid.Explanation;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import wordroid.model.R;
+
+public class AboutFragment1 extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+    Button button;
+
+    public AboutFragment1() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.about_fragment1, container, false);
+        button = (Button) rootView.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mListener != null) {
+                    mListener.sendToSecondFragment();
+                }
+            }
+        });
+        return rootView;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.sendToSecondFragment();
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void sendToSecondFragment();
+    }
+
+}
